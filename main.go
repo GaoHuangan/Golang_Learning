ee8a4466efb0de41d389639b34ee0f0b33ed77cc
HwangAn <nickelgao@yeah.net>
2025-05-26 23:41:00 +0900

    2025.05.26

--- main.go
+++ main.go
@@ -1,20 +1,13 @@
 package main
-import (
+import "LearningProject/Part8Assertion"
 
-"LearningProject/Part7Interface"
-"fmt"
-)
 
 func main() {
-
-
-	var s Part7Interface.Speaker	// Declare a variable of interface type
-	s=Part7Interface.Dog{}			// Assign a struct that implements the interface
-	fmt.Println(s.Speak())			// Output: Woof!
-
-	Part7Interface.PrintValue(123)
-	Part7Interface.PrintValue("abc")
-	Part7Interface.PrintValue(true)
-
+	Part8Assertion.PrintValue(42)
+	Part8Assertion.PrintValue("nickel")
+	p := Part8Assertion.Person{Name: "Gao"}
+	//p :=Part8Assertion.Person{Name: "Gao"}
+	Part8Assertion.WhoAreYou(p)
+	Part8Assertion.WhoAreYou("NotPerson")
 
 }
